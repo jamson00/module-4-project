ea3494cc68a883dc39671eeb1e532d2066ef1bf4
jamson00 <jameson.edward.daley@gmail.com>
2023-04-26 14:18:43 -0400

    got the question prompt to appear after clicking start, now i need the questions to shuffle

--- js/script.js
+++ js/script.js
@@ -1,63 +1,62 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
-
+const answerButton = document.getElementById('answer-buttons')
 startButton.addEventListener('click', startGame)
+answerButton.addEventListener('click', show)
+
 
 function startGame() {
     console.log('you clicked the button')
+    
+    
+    function shuffle(questions) {
+        let shuffledQuestions = [];
+        let questionIndexes = [];
+        
+        let i = 0
+        while (i < questions.length) {
+            let randomQuestions = math.floor(math.random() * questions.length)
+            if(!questionIndexes.includes(randomQuestions)) {
+                shuffledQuestions.push(questions[randomQuestions]);
+                questionIndexes.push(randomQuestions);
+                i++;
+            }
+        }
+        console.log(shuffledQuestions)
+        return shuffledQuestions
+    }
+    
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    shuffle(questions);
+    setNextQuestion()
 }
 
 function selectAnswer() {
+    const random = math.floor(math.random() * questions.length);
+    console.long(random, questions[random]);
+}
+
+function setNextQuestion() {
+    showQustion(questions.random)
 }
 
 const questions = [
     {
-        question: 'what is an array',
-        answers: [
-            {text: 'collection of similar data elements stored at contiguous memory locations', correct: true},
-            {text: 'a fish', incorrect: false},
-            {text: 'a penguin', incorrect: false},
-            {text: 'a squid', incorrect: false}
-        ]
+        question: 'what is an array?',
+        choices: ['collection of similar data elements stored at contiguous memory locations','a fish', 'a penguin', 'a squid',],
+        answer: 0
+    },  
+
+    {
+        question: 'what is 2+2?',
+        answers: ['4', 'a fish', 'a penguin', 'a squid'],
+        answer: 0
     },
 
     {
-        question: 'what is 2+2',
-        answers: [
-            {text: '4', correct: true},
-            {text: 'a fish', incorrect: false},
-            {text: 'a penguin', incorrect: false},
-            {text: 'a squid', incorrect: false}
-        ]
-    },
-
-    {
-        question: 'what is an 3+3',
-        answers: [
-            {text: '6', correct: true},
-            {text: 'a fish', incorrect: false},
-            {text: 'a penguin', incorrect: false},
-            {text: 'a squid', incorrect: false}
-        ]
-    },
-
-    {
-        question: 'what is an 4+4',
-        answers: [
-            {text: '8', correct: true},
-            {text: 'a fish', incorrect: false},
-            {text: 'a penguin', incorrect: false},
-            {text: 'a squid', incorrect: false}
-        ]
-    },
-
-    {
-        question: 'what is an 1+1',
-        answers: [
-            {text: '2', correct: true},
-            {text: 'a fish', incorrect: false},
-            {text: 'a penguin', incorrect: false},
-            {text: 'a squid', incorrect: false}
-        ]
+        question: 'what is an 3+3?',
+        answers: ['6', 'a fish', 'a penguin', 'a squid'],
+        answer: 0
     },
 ]
