9ad5e166058d0f0fbe1a38e0992f29598a813650
jamson00 <jameson.edward.daley@gmail.com>
2023-05-07 23:38:58 -0400

    got quiz to work, implemented timer

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ sources:
 https://teamtreehouse.com/community/when-i-prompt-the-questions-all-of-the-questions-i-my-array-and-answers-pop-up-in-every-prompt-box
 
 https://www.freecodecamp.org/news/how-to-create-a-countdown-timer/
+
+https://www.w3schools.com/jsref/met_node_appendchild.asp
+
+https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
--- js/script.js
+++ js/script.js
@@ -1,124 +1,118 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-let shuffledQuestions, currentQuestionIndex , correctAnswers = 0;
-
-startButton.addEventListener('click', startGame);
-//answerButtonsElement.addEventListener('click', event=> {
-    //if btn is selected it will call select answer then will determine if the users input is correct
-   // if(event.target.classList.contains('btn')) {
-      //  selectAnswer(event.target.dataset.correct);
- //   }
-//});
-
-function startGame() {
-    console.log('you clicked the button');
-    startButton.classList.add('hide');
-    //shuffling questions
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    //stopping questions from repeating
-    currentQuestionIndex = 0; 
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion();
-}
-
-
-function setNextQuestion() {
-    resetQuestion();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-
-
-
-
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-};
-
-function resetQuestion() {
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answer)
-    }
-}
-function selectAnswer(response) {
-    const question = shuffledQuestions[currentQuestionIndex]
-    //checking if user response is the correct answer
-    if(response == question.answer) {
-        correctAnswers++;
-    }
-    currentQuestionIndex++;
-    if (currentQuestionIndex < shuffledQuestions.length) {
-        setNextQuestion();
-        } else {
-            endGame();
-        }
-    
-    }
-
-const questions = [
+var questions = [
     {
-        question: 'what is an array?',
-        answers: [
-            {text: 'array', correct: true}, 
-            {text: '4', correct: false},
-            {text: 'idk', correct: false},
-            {text: 'sure', correct: false},
-        ],
-
-        question: 'what is an scope?',
-        answers: [
-            {text: 'array', correct: true}, 
-            {text: '4', correct: false},
-            {text: 'idk', correct: false},
-            {text: 'sure', correct: false},
-        ],
-
-
-        question: 'what is agile?',
-        answers: [
-            {text: 'array', correct: true}, 
-            {text: '4', correct: false},
-            {text: 'idk', correct: false},
-            {text: 'sure', correct: false},
-        ],
-
-
-        question: 'what is 2+2?',
-        answers: [
-            {text: 'array', correct: false}, 
-            {text: '4', correct: true},
-            {text: 'idk', correct: false},
-            {text: 'sure', correct: false},
-        ],
-
-
+      "question": "What does HTML stand for?",
+      "answers": ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Markup Leveler"],
+      "correctAnswer": "Hyper Text Markup Language"
     },
-]
+    {
+      "question": "What is the purpose of CSS?",
+      "answers": ["To style the structure of a web page", "To define the behavior of a web page", "To add interactivity to a web page", "To store data on a web page"],
+      "correctAnswer": "To style the structure of a web page"
+    },
+    {
+      "question": "What does JS stand for in JavaScript?",
+      "answers": ["JavaScript", "Java Source", "JumpStart", "Just Saying"],
+      "correctAnswer": "JavaScript"
+    },
+    {
+      "question": "What is the syntax for a single-line comment in Python?",
+      "answers": ["// Comment", "# Comment", "/* Comment */", "-- Comment"],
+      "correctAnswer": "# Comment"
+    },
+    {
+      "question": "What is the purpose of a variable in programming?",
+      "answers": ["To store and manipulate data", "To display text on the screen", "To connect to a database", "To define a function"],
+      "correctAnswer": "To store and manipulate data"
+    },
+    {
+      "question": "Which data structure uses a Last-In-First-Out (LIFO) approach?",
+      "answers": ["Queue", "Stack", "Tree", "Array"],
+      "correctAnswer": "Stack"
+    },
+    {
+      "question": "What is the result of 5 + '5' in JavaScript?",
+      "answers": ["10", "'55'", "55", "Error"],
+      "correctAnswer": "'55'"
+    },
+    {
+      "question": "What does the 'for' loop do?",
+      "answers": ["Executes a block of code repeatedly until a condition is met", "Declares a function", "Defines a class", "Performs arithmetic operations"],
+      "correctAnswer": "Executes a block of code repeatedly until a condition is met"
+    },
+    {
+      "question": "What does the acronym API stand for?",
+      "answers": ["Application Programming Interface", "Automated Programming Interface", "Advanced Program Integration", "Automated Protocol Interface"],
+      "correctAnswer": "Application Programming Interface"
+    },
+    {
+      "question": "What is the file extension for a Cascading Style Sheet?",
+      "answers": [".html", ".js", ".css", ".txt"],
+      "correctAnswer": ".css"
+    }
+  ]
 
-let timeLeft = 60;
-const timerElement = document.getElementById('timer-container')
+var answerIndex = 0;
+var questionEl = document.getElementById("question-container");
+var score = 0;
 
-const countdownTimer = setInterval(() => {
-    if (timeLeft > 0) {
-    timeLeft--;
-    timerElement.innerText =timeLeft;
-} else {
-    clearInterval(countdownTimer);
+function showCurrentQuestion() {
+    var currentQuestion = questions[answerIndex];
+
+    var questionHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${currentQuestion.question}?</h5>
+            <button class="btn btn-primary" onclick="showNextQuestion('${currentQuestion.answers[0]}')">${currentQuestion.answers[0]}</button>
+            <button class="btn btn-primary" onclick="showNextQuestion('${currentQuestion.answers[1]}')">${currentQuestion.answers[1]}</button>
+            <button class="btn btn-primary" onclick="showNextQuestion('${currentQuestion.answers[2]}')">${currentQuestion.answers[2]}</button>
+            <button class="btn btn-primary" onclick="showNextQuestion('${currentQuestion.answers[3]}')">${currentQuestion.answers[3]}</button>
+        </div>
+        </div>
+    `
+
+    questionEl.innerHTML = questionHTML
 }
-}, 1000);
+
+function showNextQuestion(guess) {
+    var currentQuestion = questions[answerIndex];
+
+    if(guess === currentQuestion.correctAnswer){
+        score += 10
+    }else{
+        score-=2;
+        sec-=5;
+    }
+
+    answerIndex++;
+
+    if (answerIndex < questions.length) {
+        showCurrentQuestion();
+    } else {
+        endGame()
+    }
+}
+
+function endGame() {
+    questionEl.innerHTML = `<h2>Your score is ${score}</h2>`
+}
+
+showCurrentQuestion();
+
+(function() {
+  var sec = 60;
+  function startTimer(){
+      console.log('timer suppose to go')
+      var timer = setInterval(function(){
+          sec--;
+          document.getElementById('timerDisplay').innerHTML='00:'+sec;
+          if (sec < 0) {
+              clearInterval(timer);
+              alert("Time is up!")
+          }
+      }, 1000);
+  }
+  document.getElementById('incorrect').addEventListener('click', function() {
+      document.getElementById('timerDisplay').innerHTML='00:'+sec;
+  });
+  startTimer();
+})();
